1ef9ea1b01bb922083ede2834b73825b6dbdd1be
Juan Alvarez <juan@sytex.io>
2023-08-11 10:59:16 -0300

    Missing files from commit. Adding jetbrains to .gitignore

---
+++ py_alb_analyzer/albparser.py
@@ -0,0 +1,57 @@
+import re
+
+from py_alb_analyzer.log_entry import LogEntry
+
+
+def parse_log_entry(log_entry_line: str) -> LogEntry:
+    parts = re.findall(r'(?:[^\s,"]|"(?:\\.|[^"])*")+', log_entry_line.strip())
+    parts = [p[1:-1] if p.startswith('"') and p.endswith('"') else p for p in parts]
+
+    try:
+        client_address, client_port = (
+            ("", "") if parts[3] == "-" else parts[3].split(":")
+        )
+        target_addres, target_port = (
+            ("", "") if parts[4] == "-" else parts[4].split(":")
+        )
+    except ValueError as e:
+        raise ValueError(f"Error address and port: {parts[3]} {parts[4]}") from e
+
+    try:
+        log_entry = LogEntry(
+            type=parts[0],
+            time=parts[1],
+            elb=parts[2],
+            client_address=client_address,
+            client_port=client_port,
+            target_address=target_addres,
+            target_port=target_port,
+            request_processing_time=float(parts[5]),
+            target_processing_time=float(parts[6]),
+            response_processing_time=float(parts[7]),
+            elb_status_code=int(parts[8]),
+            target_status_code=int(parts[9]) if parts[9] != "-" else None,
+            received_bytes=int(parts[10]),
+            sent_bytes=int(parts[11]),
+            request=parts[12],
+            user_agent=parts[13],
+            ssl_cipher=parts[14],
+            ssl_protocol=parts[15],
+            target_group_arn=parts[16],
+            trace_id=parts[17],
+            domain_name=parts[18],
+            chosen_cert_arn=parts[19],
+            matched_rule_priority=None if parts[20] == "-" else int(parts[20]),
+            request_creation_time=parts[21],
+            actions_executed=parts[22],
+            redirect_url=parts[23],
+            error_reason=parts[24],
+            target_port_list=parts[25],
+            target_status_code_list=parts[26],
+            classification=parts[27],
+            classification_reason=parts[28],
+        )
+    except ValueError as e:
+        raise ValueError(f"Error parsing log entry: {log_entry_line}") from e
+
+    return log_entry
---
+++ py_alb_analyzer/log_entry.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class LogEntry:
+    type: str
+    time: str
+    elb: str
+    client_address: str
+    client_port: str
+    target_address: str
+    target_port: str
+    request_processing_time: float
+    target_processing_time: float
+    response_processing_time: float
+    elb_status_code: int
+    target_status_code: Optional[int]
+    received_bytes: int
+    sent_bytes: int
+    request: str
+    user_agent: str
+    ssl_cipher: str
+    ssl_protocol: str
+    target_group_arn: str
+    trace_id: str
+    domain_name: str
+    chosen_cert_arn: str
+    matched_rule_priority: int
+    request_creation_time: str
+    actions_executed: str
+    redirect_url: str
+    error_reason: str
+    target_port_list: str
+    target_status_code_list: str
+    classification: str
+    classification_reason: str
